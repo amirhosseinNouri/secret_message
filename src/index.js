59eceafedf4b979f8c943807b39847cb84236575
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-04 12:43:03 +0330

    encrypting added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.querySelector("form").addEventListener("submit" , event =>{
     event.preventDefault()
-    const input = document.querySelector("input")
-    console.log(input.value);
+    const input = document.querySelector("#message-input")
+    const ecrypted = btoa(input.value)
+    document.querySelector("#link-input").value = ecrypted
 })
