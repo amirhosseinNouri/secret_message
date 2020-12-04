513c587c34e5fb665d6f00e0fe1ca4dd7e224d92
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-04 13:11:41 +0330

    showing secret message done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-document.querySelector("form").addEventListener("submit" , event =>{
-    event.preventDefault()
+const { hash } = window.location;
 
-    document.querySelector("#message-form").classList.add("hide")
-    document.querySelector("#link-form").classList.remove("hide")
+const message = atob(hash.replace(/#/g, ""));
+if (message) {
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("h1").innerHTML = message
+}
 
-    const input = document.querySelector("#message-input")
-    const encrypted = btoa(input.value)
-    const linkInput = document.querySelector("#link-input");
-    linkInput.value = `${window.location}#${encrypted}`
-    linkInput.select()
-})
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
