0773fa0e5e191c0c3b13bf2a845f3f9da2b4c706
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-04 12:49:20 +0330

    generating url added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 document.querySelector("form").addEventListener("submit" , event =>{
     event.preventDefault()
     const input = document.querySelector("#message-input")
-    const ecrypted = btoa(input.value)
-    document.querySelector("#link-input").value = ecrypted
+    const encrypted = btoa(input.value)
+    const linkInput = document.querySelector("#link-input");
+    linkInput.value = `${window.location}#${encrypted}`
+    linkInput.select()
 })
